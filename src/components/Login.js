309720f94f29ab42820cfdf435b45b86380bea5f
AlfonsoTorrez <alftorres@csumb.edu>
2020-07-02 21:06:00 -0700

    feat: set authed user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
 
 class Login extends Component {
+
+  state = {
+    selectedName: ''
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({selectedName: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    //Setting the User
+    e.preventDefault()
+    this.props.dispatch(setAuthedUser(this.state.selectedName))
+  }
+
   render() {
     console.log(this.props)
     return (
       <div>
         <h2>Welcome to the Would You Rather App!</h2>
         <p>Please sign in to continue</p>
-        <select>
-          <option value="User" disabled selected>Select a User</option>
+        <form onSubmit={this.handleSubmit} >
+        <select onChange={this.handleChange}>
+          <option value="N/A" disabled selected>Select a User</option>
           {this.props.myUsers.map((user) => (
             <option value={user.name}>{user.name}</option>
           ))}
         </select>
-        <div>
-          <button>
-            Sign In
-          </button>
-        </div>
+          <div>
+            <button type='submit'>
+              Sign In
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
